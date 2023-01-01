55583379139a9809f4f9732ded283d4dd9486fe6
steveandreassend <119098920+steveandreassend@users.noreply.github.com>
2023-01-01 22:17:50 +0100

    Create install.sql

---
+++ install.sql
@@ -0,0 +1,16 @@
+WHENEVER SQLERROR EXIT 1;
+WHENEVER OSERROR EXIT 1;
+
+CONNECT / AS SYSDBA
+
+PROMPT dbms_partition_ranger_sec: Security
+@dbms_partition_ranger_sec
+
+PROMPT dbms_partition_ranger_tab: Tables
+@dbms_partition_ranger_tab
+
+PROMPT dbms_partition_ranger_spec: Package Specification
+@dbms_partition_ranger_spec
+
+PROMPT dbms_partition_ranger_body: Package Body
+@dbms_partition_ranger_body
